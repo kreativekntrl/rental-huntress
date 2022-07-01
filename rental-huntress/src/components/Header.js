7ca1f4ec3bf032d3c6a9b0eb20833ed7de0853f5
Ava <avamsteimle@gmail.com>
2022-06-30 20:11:24 -0700

    header content

--- rental-huntress/src/components/Header.js
+++ rental-huntress/src/components/Header.js
@@ -1,8 +1,19 @@
 const Header = () => {
-    return(
-        <div>
-            
-        </div>
-    );
+    return (
+<div className="dropdown-container">
+<div className="dropdown">
+  <button className="btn menu-btn" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style={{padding: "0", float: "left"}}>
+  <i className="bi bi-list" style={{fontSize: "30px"}}></i>
+  </button>
+  <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
+    <button className="dropdown-item" type="button">Action</button>
+    <button className="dropdown-item" type="button">Another action</button>
+    <button className="dropdown-item" type="button">Something else here</button>
+  </div>
+</div>
+<a href="#" style={{float: "right", color: "#4c4c4c", textDecoration: "none"}}>Sign Up / Sign In</a>
+<h1>Rental Huntress</h1>
+</div>
+);
 }
 export default Header;
