f893f9c8284616a3a5842cadc33143cc50428722
John Yu <johnyu.programmer@gmail.com>
2022-07-03 12:08:04 -0700

    format commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import './App.scss';
-import Header from './components/Header.js';
-import Nav from './components/Nav.js';
-import Footer from './components/Footer.js';
-import Aside from './components/Aside.js';
-import Main from './components/Main.js';
-;
+import "./App.scss";
+import Header from "./components/Header.js";
+import Nav from "./components/Nav.js";
+import Footer from "./components/Footer.js";
+import Aside from "./components/Aside.js";
+import Main from "./components/Main.js";
 function App() {
   return (
-<div className="page-wrap">
-  <header className="page-header">
-    <Header />
-  </header>
-  <nav className="page-nav">
-    <Nav className="page-nav"/>
-  </nav>
-  <main className="page-main">
-    <Main />
-  </main>
-  <aside className="page-sidebar">
-    <Aside />
-  </aside>
-  <footer className="page-footer">
-    <Footer />
-  </footer>
-</div>
-
+    <div className="page-wrap">
+      <header className="page-header">
+        <Header />
+      </header>
+      <nav className="page-nav">
+        <Nav className="page-nav" />
+      </nav>
+      <main className="page-main">
+        <Main />
+      </main>
+      <aside className="page-sidebar">
+        <Aside />
+      </aside>
+      <footer className="page-footer">
+        <Footer />
+      </footer>
+    </div>
   );
 }
 
