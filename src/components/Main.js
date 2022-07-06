0d46d4a41bc84a4414741bd7419e0d6de57909a7
Ava <avamsteimle@gmail.com>
2022-07-06 10:19:47 -0700

    feat: I implemented a search engine that toggles rental results based on San Diego and Los Angeles

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,8 +3,10 @@ import img2 from "../images/building2.jpg";
 import img3 from "../images/building3.jpg";
 import img4 from "../images/building4.jpg";
 import Jumbotron from "./Jumbotron";
+import LaList from './LaList';
+import SdList from './SdList';
 
-const Main = () => {
+const Main = ({userInput}) => {
   const allImages = [img1, img2, img3, img4];
   const allStates = [
     "Alabama",
@@ -76,60 +78,14 @@ const Main = () => {
 
   return (
     <div>
-      <h2>Explore Rentals in San Diego, CA</h2>
-      <div className="card-group">
-        <div className="card" style={{ width: "15rem" }}>
-          <img className="card-img-top" src={img1} alt="Card cap"></img>
-          <div className="card-body">
-            <h5 className="card-title">The High</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              See Rental
-            </a>
-          </div>
-        </div>
-        <div className="card" style={{ width: "15rem" }}>
-          <img className="card-img-top" src={img2} alt="Card cap"></img>
-          <div className="card-body">
-            <h5 className="card-title">Lamark</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              See Rental
-            </a>
-          </div>
-        </div>
-        <div className="card" style={{ width: "15rem" }}>
-          <img className="card-img-top" src={img3} alt="Card image cap"></img>
-          <div className="card-body">
-            <h5 className="card-title">La Mesa Heights</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="" className="btn btn-primary">
-              See Rental
-            </a>
-          </div>
-        </div>
-        <div className="card" style={{ width: "15rem" }}>
-          <img className="card-img-top" src={img4} alt="Card cap"></img>
-          <div className="card-body">
-            <h5 className="card-title">758 Broadway</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-            <a href="#" className="btn btn-primary">
-              See Rental
-            </a>
-          </div>
-        </div>
+      <div>
+        {(() => {
+          if (userInput === "Los Angeles") {
+            return <LaList />;
+          } else {
+            return <SdList />;
+          }
+        })()}
       </div>
       <a
         href="#"
